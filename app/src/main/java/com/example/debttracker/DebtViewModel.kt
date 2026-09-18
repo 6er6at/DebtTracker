43702ace32dd787218d39a964f3f26b99ca1b125
Kirill <berbatkirill@gmail.com>
2026-09-18 20:46:16 +0400

    Implement basic debt CRUD

--- app/src/main/java/com/example/debttracker/DebtViewModel.kt
+++ app/src/main/java/com/example/debttracker/DebtViewModel.kt
@@ -7,15 +7,18 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class DebtViewModel(application: Application) : AndroidViewModel(application)
-{
+class DebtViewModel(application: Application) : AndroidViewModel(application) {
+
     private val _debts = MutableStateFlow<List<Debt>>(emptyList())
+
     val debts: StateFlow<List<Debt>> = _debts
+
     private val database = DatabaseProvider.getDatabase(
         application
     )
 
     private val debtDao = database.debtDao()
+
     init {
         viewModelScope.launch {
             _debts.value = debtDao.getAllDebts()
@@ -25,6 +28,21 @@ class DebtViewModel(application: Application) : AndroidViewModel(application)
     fun addDebt(debt: Debt) {
         viewModelScope.launch {
             debtDao.insertDebt(debt)
+            _debts.value = debtDao.getAllDebts()
+        }
+    }
+
+    fun updateDebt(debt: Debt) {
+        viewModelScope.launch {
+            debtDao.updateDebt(debt)
+            _debts.value = debtDao.getAllDebts()
+        }
+    }
+
+    fun deleteDebt(debt: Debt) {
+        viewModelScope.launch {
+            debtDao.deleteDebt(debt)
+            _debts.value = debtDao.getAllDebts()
         }
     }
 }
